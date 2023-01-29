3a01dccbc36502ab6f96a20a15456b9828ffd075
kth990303 <kth990303@naver.com>
2023-01-29 21:27:49 +0900

    style: 출석여부 네이밍 변경

--- src/main/kotlin/nexters/admin/domain/attendance/AttendanceStatus.kt
+++ src/main/kotlin/nexters/admin/domain/attendance/AttendanceStatus.kt
@@ -2,8 +2,8 @@ package nexters.admin.domain.attendance
 
 enum class AttendanceStatus(val value: String) {
     PENDING("대기"),
-    ATTEND("출석"),
-    TARDY("지각"),    // TODO: 미국에서는 TARDY 라고 하나, 우리가 알아보기엔 LATE 가 편함. 네이밍 고민해보자
+    ATTENDED("출석"),
+    TARDY("지각"),
     UNAUTHORIZED_ABSENCE("무단결석"),
     AUTHORIZED_ABSENCE("통보결석"),
 }
