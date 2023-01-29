818bec6d42dadbe71cc2f7b80d13fbf7398f8d4b
kth990303 <kth990303@naver.com>
2023-01-29 22:06:25 +0900

    test: 비밀번호 암호화 단위테스트 작성

---
+++ src/test/kotlin/nexters/admin/domain/member/PasswordTest.kt
@@ -0,0 +1,14 @@
+package nexters.admin.domain.member
+
+import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.Test
+
+class PasswordTest {
+
+    @Test
+    fun `비밀번호 암호화`() {
+        val password = Password("1234")
+
+        password.value shouldNotBe "1234"
+    }
+}
