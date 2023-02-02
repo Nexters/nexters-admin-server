2d714b1ff21458193b0cac19eca62455054e5aa9
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-02 16:16:52 +0900

    feat: 비밀번호 수정 API 구현

--- src/main/kotlin/nexters/admin/controller/user/MemberController.kt
+++ src/main/kotlin/nexters/admin/controller/user/MemberController.kt
@@ -1,7 +1,10 @@
 package nexters.admin.controller.user
 
+import nexters.admin.common.auth.LoggedInMember
+import nexters.admin.domain.user.member.Member
 import nexters.admin.service.auth.AuthService
 import nexters.admin.service.auth.LoginRequest
+import nexters.admin.service.user.MemberService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,6 +16,7 @@ import javax.validation.Valid
 @RestController
 class MemberController(
         private val authService: AuthService,
+        private val memberService: MemberService,
 ) {
     // TODO: erase all API JavaDoc
     /**
@@ -29,5 +33,20 @@ class MemberController(
     fun login(@RequestBody @Valid request: LoginRequest): ResponseEntity<TokenResponse> {
         val token = authService.generateMemberToken(request)
         return ResponseEntity.ok(TokenResponse(token))
+    }
+
+    /**
+     * PUT /api/users/password
+     * Authorization: Bearer valid.jwt.token
+     * RequestBody {
+     *      password: string
+     * }
+     */
+    @PostMapping("/password")
+    fun changePassword(
+            @LoggedInMember member: Member,
+            @RequestBody @Valid request: UpdatePasswordRequest): ResponseEntity<Void> {
+        memberService.updatePassword(member, request.password)
+        return ResponseEntity.ok().build()
     }
 }
