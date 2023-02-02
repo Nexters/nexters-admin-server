ded48b15742f7a80c759b395e4593ea87a1c7952
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-02 14:45:29 +0900

    style: 커스텀 예외 클래스들 사이에 빈 줄 추가
    - 불필요한 타입 명시 제거

--- src/main/kotlin/nexters/admin/common/exception/CustomExceptions.kt
+++ src/main/kotlin/nexters/admin/common/exception/CustomExceptions.kt
@@ -1,10 +1,13 @@
 package nexters.admin.common.exception
 
 class BadRequestException(message: String?) : RuntimeException(message)
+
 class UnauthenticatedException(message: String?) : RuntimeException(message) {
     companion object {
-        fun loginFail(): UnauthenticatedException = UnauthenticatedException("로그인에 실패하였습니다.")
+        fun loginFail() = UnauthenticatedException("로그인에 실패하였습니다.")
     }
 }
+
 class ForbiddenException(message: String?) : RuntimeException(message)
+
 class NotFoundException(message: String?) : RuntimeException(message)
