18663aca20f83b340dc62cfc4133d892f4d78651
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-02 16:07:57 +0900

    feat: JwtTokenProvider에 토큰에서 이메일 추출하는 메서드 구현

--- src/main/kotlin/nexters/admin/common/exception/CustomExceptions.kt
+++ src/main/kotlin/nexters/admin/common/exception/CustomExceptions.kt
@@ -5,6 +5,7 @@ class BadRequestException(message: String?) : RuntimeException(message)
 class UnauthenticatedException(message: String?) : RuntimeException(message) {
     companion object {
         fun loginFail() = UnauthenticatedException("로그인에 실패하였습니다.")
+        fun loginNeeded() = UnauthenticatedException("다시 로그인해주시기 바랍니다.")
     }
 }
 
--- src/main/kotlin/nexters/admin/service/auth/JwtTokenProvider.kt
+++ src/main/kotlin/nexters/admin/service/auth/JwtTokenProvider.kt
@@ -1,7 +1,9 @@
 package nexters.admin.service.auth
 
 import io.jsonwebtoken.Claims
+import io.jsonwebtoken.JwtException
 import io.jsonwebtoken.Jwts
+import nexters.admin.common.exception.UnauthenticatedException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.Date
@@ -22,4 +24,21 @@ class JwtTokenProvider(
                 .signWith(secretKey)
                 .compact()
     }
+
+    fun getPayload(token: String): String {
+        try {
+            return extractPayload(token)
+        } catch (e: JwtException) {
+            throw UnauthenticatedException.loginNeeded()
+        } catch (e: IllegalArgumentException) {
+            throw UnauthenticatedException.loginNeeded()
+        }
+    }
+
+    private fun extractPayload(token: String) = Jwts.parserBuilder()
+            .setSigningKey(secretKey.encoded)
+            .build()
+            .parseClaimsJws(token)
+            .body
+            .subject
 }
