c6e74a7071925fcc79d1497d0ede114cfcfb4947
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-10 00:10:21 +0900

    feat: 어드민 로그인에 따른 토큰 발급 서비스 메서드 구현

--- src/main/kotlin/nexters/admin/service/auth/AuthDtos.kt
+++ src/main/kotlin/nexters/admin/service/auth/AuthDtos.kt
@@ -3,6 +3,11 @@ package nexters.admin.service.auth
 import nexters.admin.domain.user.Password
 import javax.validation.constraints.Email
 
+data class AdminLoginRequest(
+        val username: String,
+        val password: Password
+)
+
 data class LoginRequest(
         @field:Email
         val email: String,
