f31435ecdd22dddce35e97591647ebefd471d86a
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-12 22:46:31 +0900

    docs: Swagger에서 @LoggedInMember 붙은 매개변수 숨기기
    - 어노테이션 자체에 @Parameter(hidden = true) 추가

--- src/main/kotlin/nexters/admin/support/auth/AuthAnnotations.kt
+++ src/main/kotlin/nexters/admin/support/auth/AuthAnnotations.kt
@@ -1,5 +1,8 @@
 package nexters.admin.support.auth
 
+import io.swagger.v3.oas.annotations.Parameter
+
+@Parameter(hidden = true)
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class LoggedInMember
