a585022defb301999a3d337331f96b07ee621ab0
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-18 20:01:20 +0900

    feat: 자동으로 한국을 기준으로 타임존 사용하도록 설정

--- src/main/kotlin/nexters/admin/ServerApplication.kt
+++ src/main/kotlin/nexters/admin/ServerApplication.kt
@@ -2,9 +2,17 @@ package nexters.admin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class ServerApplication
+class ServerApplication {
+
+    @PostConstruct
+    fun started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
