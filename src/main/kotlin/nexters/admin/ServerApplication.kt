866be5a2ef2aefb82cca173887d078f787fce6c8
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-19 15:54:24 +0900

    refactor: 별도의 Configuration 빈에서 타임존 설정

--- src/main/kotlin/nexters/admin/ServerApplication.kt
+++ src/main/kotlin/nexters/admin/ServerApplication.kt
@@ -2,17 +2,9 @@ package nexters.admin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.util.TimeZone
-import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class ServerApplication {
-
-    @PostConstruct
-    fun started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
-}
+class ServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
