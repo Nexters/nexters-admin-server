ac40f4b1531ee9dcdc3860d8d4cbcb97a10040cf
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-20 22:54:24 +0900

    config: CORS 셋업

---
+++ src/main/kotlin/nexters/admin/config/CorsConfig.kt
@@ -0,0 +1,15 @@
+package nexters.admin.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+    }
+}
