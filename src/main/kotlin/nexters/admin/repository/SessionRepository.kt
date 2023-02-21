e4a49110875095c5bb48d2a16c89c0b21f47ac0b
xxeol2 <jhj01177@naver.com>
2023-02-21 22:29:00 +0900

    fix: 당일 세션 조회안되는 오류 수정

--- src/main/kotlin/nexters/admin/repository/SessionRepository.kt
+++ src/main/kotlin/nexters/admin/repository/SessionRepository.kt
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDate
 
 fun SessionRepository.findUpcomingSession(generation: Int, today: LocalDate): Session? {
-    return findTopByGenerationAndSessionTimeIsAfterOrderBySessionTimeAsc(generation, today)
+    return findTopByGenerationAndSessionTimeGreaterThanEqualOrderBySessionTimeAsc(generation, today)
 }
 
 interface SessionRepository : JpaRepository<Session, Long> {
     fun findAllByGeneration(generation: Int): List<Session>
 
-    fun findTopByGenerationAndSessionTimeIsAfterOrderBySessionTimeAsc(generation: Int, today: LocalDate): Session?
+    fun findTopByGenerationAndSessionTimeGreaterThanEqualOrderBySessionTimeAsc(generation: Int, today: LocalDate): Session?
 }
