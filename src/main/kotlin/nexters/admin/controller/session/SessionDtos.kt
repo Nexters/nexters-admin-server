dc685632897f4dfe149955ac48d9a771b2efe2fd
xxeol2 <jhj01177@naver.com>
2023-02-21 22:38:01 +0900

    fix: 세션 생성 및 업데이트시 description과 message nullable하게 변경

--- src/main/kotlin/nexters/admin/controller/session/SessionDtos.kt
+++ src/main/kotlin/nexters/admin/controller/session/SessionDtos.kt
@@ -5,8 +5,8 @@ import java.time.LocalDateTime
 
 data class CreateSessionRequest(
         val title: String,
-        val description: String,
-        val message: String,
+        val description: String?,
+        val message: String?,
         val generation: Int,
         val sessionTime: LocalDate,
         val week: Int,
@@ -16,8 +16,8 @@ data class CreateSessionRequest(
 
 data class UpdateSessionRequest(
         val title: String,
-        val description: String,
-        val message: String,
+        val description: String?,
+        val message: String?,
         val generation: Int,
         val sessionTime: LocalDate,
         val week: Int,
