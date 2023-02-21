82101b168124b63298e89b461ce6d157761e670a
Jeong Jinwoo <jwjeong96@gmail.com>
2023-02-21 22:54:25 +0900

    fix: 1분마다 스케줄러 동작하도록 cron 문법 수정 및 @EnableScheduling 추가

--- src/main/kotlin/nexters/admin/scheduler/QrScheduler.kt
+++ src/main/kotlin/nexters/admin/scheduler/QrScheduler.kt
@@ -9,7 +9,7 @@ class QrScheduler(
         private val qrCodeRepository: QrCodeRepository
 ) {
     // TODO: 동시성 이슈 나중에 해결 필요 - 스케줄러가 동작할 때 관리자에 의해 initializeCodes 메서드 호출되면 데이터가 잘못 덮어씌어질 수 있음.
-    @Scheduled(cron = "* * * * * ?", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
     fun runEveryMinute() {
         qrCodeRepository.updateValidCodes()
     }
