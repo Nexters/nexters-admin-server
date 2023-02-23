9f2593cf3d5f25669eabf9a4d940c33610990593
kth990303 <kth990303@naver.com>
2023-02-23 17:36:52 +0900

    fix: `HandlerArgumentResolver`가 등록되지 않았던 버그 수정

--- src/main/kotlin/nexters/admin/config/WebMvcConfig.kt
+++ src/main/kotlin/nexters/admin/config/WebMvcConfig.kt
@@ -1,11 +1,19 @@
 package nexters.admin.config
 
+import nexters.admin.support.auth.LoginUserResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class CorsConfig: WebMvcConfigurer {
+class WebMvcConfig(
+        val loginUserResolver: LoginUserResolver,
+) : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(loginUserResolver)
+    }
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
