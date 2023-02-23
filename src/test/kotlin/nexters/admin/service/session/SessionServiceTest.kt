28daa093ddba02cb7cfc662711ab681ee3af9599
xxeol2 <jhj01177@naver.com>
2023-02-23 18:55:00 +0900

    refactor: sessionServiceTest Fixture 사용해서 리팩토링

--- src/test/kotlin/nexters/admin/service/session/SessionServiceTest.kt
+++ src/test/kotlin/nexters/admin/service/session/SessionServiceTest.kt
@@ -49,47 +49,26 @@ class SessionServiceTest(
 
     @Test
     fun `세션 조회`() {
-        val id = sessionService.createSession(
-                CreateSessionRequest(
-                        title = "Test title",
-                        description = "Test description",
-                        message = "Test message",
-                        generation = 22,
-                        sessionTime = LocalDate.now(),
-                        week = 3,
-                )
-        )
+        val session = createNewSession(title = "Test title")
+        sessionRepository.save(session)
 
-        val found = sessionService.findSession(id)
+        val found = sessionService.findSession(session.id)
 
         found?.title shouldBe "Test title"
     }
 
     @Test
     fun `특정 기수의 세션 조회`() {
-        sessionService.createSession(
-                CreateSessionRequest(
-                        title = "Test title0",
-                        description = "Test description",
-                        message = "Test message",
-                        generation = 22,
-                        sessionTime = LocalDate.now(),
-                        week = 2,
-                )
-        )
-        sessionService.createSession(
-                CreateSessionRequest(
-                        title = "Test title1",
-                        description = "Test description",
-                        message = "Test message",
-                        generation = 22,
-                        sessionTime = LocalDate.now(),
-                        week = 3,
-                )
-        )
+        val session1 = createNewSession(title = "Test title1", generation = 22)
+        val session2 = createNewSession(title = "Test title2", generation = 22)
+        val session3 = createNewSession(title = "Test title3", generation = 23)
+        sessionRepository.save(session1)
+        sessionRepository.save(session2)
+        sessionRepository.save(session3)
 
         val founds = sessionService.findSessionByGeneration(22)
 
+        founds.size shouldBe 2
         founds.forEach {
             it.generation shouldBe 22
         }
@@ -117,20 +96,12 @@ class SessionServiceTest(
 
     @Test
     fun `세션 삭제`() {
-        val id = sessionService.createSession(
-                CreateSessionRequest(
-                        title = "Test title",
-                        description = "Test description",
-                        message = "Test message",
-                        generation = 22,
-                        sessionTime = LocalDate.now(),
-                        week = 3,
-                )
-        )
+        val session = createNewSession()
+        sessionRepository.save(session)
 
-        sessionService.deleteSession(id)
+        sessionService.deleteSession(session.id)
 
-        val found = sessionRepository.findByIdOrNull(id)
+        val found = sessionRepository.findByIdOrNull(session.id)
 
         found shouldBe null
     }
